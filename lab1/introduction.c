381671fefc13ea4917bf8eb9072a633712c12394
anuzzz7 <meb25011@tezu.ac.in>
2025-11-21 23:30:44 +0530

    Add files via upload

---
+++ lab1/introduction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Anuj Adhikari.\n");
+    printf("My branch is Mechanical Engineering.\n");
+    printf("My roll no is MEB25013.\n");
+
+return 0;
+}
